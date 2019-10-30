84b5f37e8b2852a17b2e996c8a07883a77985125
Randy Steele <steelerx155@gmail.com>
2019-10-30 20:47:12 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,11 @@
-
+class CashRegister
+  attr_accessor :items, :discount, :total, :last_transaction
+  
+  def initialize(discount = 0)
+    @total = 0 
+    @discount = discount
+    @items = []
+ end
+ 
+ 
+end 
